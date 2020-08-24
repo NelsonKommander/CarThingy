e499828b8c34b56ddc22b1f58826179ddabcef2e
Nelson Ribeiro Reis Filho <47700356+NelsonKommander@users.noreply.github.com>
2020-08-24 15:00:44 -0300

    Front of the vehicle card added
    Signed-off-by: Nelson Ribeiro Reis Filho <47700356+NelsonKommander@users.noreply.github.com>

--- lib/screens/vehicles.dart
+++ lib/screens/vehicles.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 // bla bla bla
 class VehiclesPage extends StatefulWidget {
   @override
@@ -11,14 +12,63 @@ class _VehiclesPageState extends State<VehiclesPage> {
     return Scaffold(
         appBar: AppBar(
           backgroundColor: Theme.of(context).primaryColor,
-          title: Text("Veiculos"),
+          title: Text("Veículos"),
           actions: <Widget>[Icon(Icons.add, color: Colors.white)],
         ),
         body: Container(
-          color: Colors.grey[900],
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          decoration: new BoxDecoration(color: Colors.grey[900]),
           child: Column(
-            children: <Widget>[],
+            children: <Widget>[FrontCardFace(), Text("como vai você?")],
           ),
         ));
+  }
+}
+
+class FrontCardFace extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.grey[700],
+      margin: EdgeInsets.fromLTRB(15, 15, 15, 0),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Icon(Icons.directions_car, size: 48, color: Colors.white),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("Veículo", style: Theme.of(context).textTheme.bodyText1),
+              Text("Gol", style: Theme.of(context).textTheme.bodyText2),
+              Text("Ano", style: Theme.of(context).textTheme.bodyText1),
+              Text("2009", style: Theme.of(context).textTheme.bodyText2)
+            ],
+          ),
+          Padding(
+              padding: EdgeInsets.fromLTRB(
+                  0, 40, MediaQuery.of(context).size.width * 0.36, 40)),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("Placa", style: Theme.of(context).textTheme.bodyText1),
+              Text("XYZ-1234", style: Theme.of(context).textTheme.bodyText2),
+              Text("Combustível", style: Theme.of(context).textTheme.bodyText1),
+              Text("Flex", style: Theme.of(context).textTheme.bodyText2)
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class BackCardFace extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return Card();
   }
 }
